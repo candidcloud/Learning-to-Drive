69ec43f76abf28689ad438bdbfc6b97fa08a6802
Patrick Rosse <pat.j.rosse@gmail.com>
2019-04-10 09:25:23 -0600

    naming conventions

--- q_racer.py
+++ q_racer.py
@@ -1,112 +1,51 @@
-import os
-import pygame
-from math import tan, radians, degrees, copysign
-from pygame.math import Vector2
+"""
+Main script which implements Q-learning to teach a computer how to drive
+around a track quickly.
+"""
 
+class Game():
+    """
+    A class which maintains state, action, reward, policy, and Q variables as
+    a game simulation progresses using the class methods provided.
+    """
 
-class Car:
-    def __init__(self, x, y, angle=0.0, length=4, max_steering=30, max_acceleration=5.0):
-        self.position = Vector2(x, y)
-        self.velocity = Vector2(0.0, 0.0)
-        self.angle = angle
-        self.length = length
-        self.max_acceleration = max_acceleration
-        self.max_steering = max_steering
-        self.max_velocity = 20
-        self.brake_deceleration = 10
-        self.free_deceleration = 5
+    def __init__():
+        state =  # this will be the track with available rewards
+        action_parameters =  # tuple or list
+        Q =  # dictionary
+        policy =  # function
+        reward = 0  # accumulates rewards
 
-        self.acceleration = 0.0
-        self.steering = 0.0
+    def get_action(state, Q, policy, action_parameters):
 
-    def update(self, dt):
-        self.velocity += (self.acceleration * dt, 0)
-        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
+        return action
 
-        if self.steering:
-            turning_radius = self.length / tan(radians(self.steering))
-            angular_velocity = self.velocity.x / turning_radius
-        else:
-            angular_velocity = 0
+    def evolve(state, action):
 
-        self.position += self.velocity.rotate(-self.angle) * dt
-        self.angle += degrees(angular_velocity) * dt
+        return next_state
 
+    def get_reward(state, next_state, action):
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        pygame.display.set_caption("Car tutorial")
-        width = 1280
-        height = 720
-        self.screen = pygame.display.set_mode((width, height))
-        self.clock = pygame.time.Clock()
-        self.ticks = 60
-        self.exit = False
+        return reward
 
-    def run(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        image_path = os.path.join(current_dir, "car.png")
-        car_image = pygame.image.load(image_path)
-        car = Car(0, 0)
-        ppu = 32
+    def learn(state, next_state, action, Q, reward):
 
-        while not self.exit:
-            dt = self.clock.get_time() / 1000
+        return Q
 
-            # Event queue
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.exit = True
+    def check_end(next_state):
 
-            # User input
-            pressed = pygame.key.get_pressed()
+        return is_end
 
-            if pressed[pygame.K_UP]:
-                if car.velocity.x < 0:
-                    car.acceleration = car.brake_deceleration
-                else:
-                    car.acceleration += 1 * dt
-            elif pressed[pygame.K_DOWN]:
-                if car.velocity.x > 0:
-                    car.acceleration = -car.brake_deceleration
-                else:
-                    car.acceleration -= 1 * dt
-            elif pressed[pygame.K_SPACE]:
-                if abs(car.velocity.x) > dt * car.brake_deceleration:
-                    car.acceleration = -copysign(car.brake_deceleration, car.velocity.x)
-                else:
-                    car.acceleration = -car.velocity.x / dt
-            else:
-                if abs(car.velocity.x) > dt * car.free_deceleration:
-                    car.acceleration = -copysign(car.free_deceleration, car.velocity.x)
-                else:
-                    if dt != 0:
-                        car.acceleration = -car.velocity.x / dt
-            car.acceleration = max(-car.max_acceleration, min(car.acceleration, car.max_acceleration))
-
-            if pressed[pygame.K_RIGHT]:
-                car.steering -= 30 * dt
-            elif pressed[pygame.K_LEFT]:
-                car.steering += 30 * dt
-            else:
-                car.steering = 0
-            car.steering = max(-car.max_steering, min(car.steering, car.max_steering))
-
-            # Logic
-            car.update(dt)
-
-            # Drawing
-            self.screen.fill((0, 0, 0))
-            rotated = pygame.transform.rotate(car_image, car.angle)
-            rect = rotated.get_rect()
-            self.screen.blit(rotated, car.position * ppu - (rect.width / 2, rect.height / 2))
-            pygame.display.flip()
-
-            self.clock.tick(self.ticks)
-        pygame.quit()
-
-
-if __name__ == '__main__':
+# Example of how the game should be played
+while True:
     game = Game()
-    game.run()
+    action = game.get_action(game.state, game.Q, game.policy, game.action_parameters)
+    next_state = game.evolve(game.state, action)
+    reward = game.get_reward(game.state, next_state, action)
+    game.Q = game.learn(game.state, next_state, action, game.Q, reward)
+    game.reward += reward
+    if game.check_end(next_state):
+        print(f"End of Game: Total reward is {game.reward}.")
+        break
+    else:
+        game.state = next_state
