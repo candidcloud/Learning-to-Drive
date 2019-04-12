a2cbbcd212a4d015337906a1c739149ff7180d73
Codie <black.nova.13.5@gmail.com>
2019-04-12 02:58:06 -0600

    Create find_window.py
    A test script which locates a desired window. To be used for centering the AI window on the game.

---
+++ find_window.py
@@ -0,0 +1,34 @@
+"""
+A script to locate a window with title entered by the user.
+For q_racer, this is "Go Q-Racer, go Q-Racer, go!" w/o quotes.
+"""
+
+import win32gui
+
+def get_dimensions(window):
+    """
+    Grabs the dimensions of a given window. For an unknown reason, it is
+    necessary to multiply the received dimensions by numbers close to 2.
+    """
+    rect = win32gui.GetWindowRect(window)
+    x = 2*rect[0]
+    y = 2*rect[1]
+    w = 2*rect[2]
+    h = 2*rect[3]
+    print(f"Window {win32gui.GetWindowText(window)}: ")
+    print(f"\tLocation: {(x, y)}")
+    print(f"\t    Size: {(w, h)}")
+
+def main():
+    """
+    Prompt the user for the title of the desired window and print the
+    dimensions.
+    """
+    win2find = input('Enter window title: ')
+    window = win32gui.FindWindowEx(None, None, None, win2find)
+    if not (window == 0):
+        get_dimensions(window)
+
+
+if __name__ == '__main__':
+    main()
